7b8364612ae9102fa7a8dcdc60deb6fcc3832059
Monique Chang <moniquechang1997@gmail.com>
2022-07-19 21:21:16 +0000

    #1 added data from form to local storage

--- js/data.js
+++ js/data.js
@@ -6,3 +6,10 @@ var data = {
   editing: null,
   nextEntryId: 1
 };
+
+window.addEventListener('beforeunload', handleBeforeunload);
+
+function handleBeforeunload(event) {
+  var $data = JSON.stringify(data);
+  localStorage.setItem('code-journal-local-storage', $data);
+}
